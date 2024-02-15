0466fdc9d6ff0b1fa305a3f3b2881db4e7889e15
William Park <l.jimenez.leon97@gmail.com>
2024-02-15 12:25:06 -0600

    Jr added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ export const Header = () => {
 
         <div className="header-titulo">
           <h1 className="nombre">Luis Enrique Jiménez León</h1>
-          <h2 className="puesto">Desarrollador Full Stack</h2>
+          <h2 className="puesto">Desarrollador Full Stack Jr.</h2>
 
           <a className="cv" href="./document/cv-luis.pdf" target="_blank">Descargar CV</a>
         </div>
