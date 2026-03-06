e01e5d4fbd9ea765c897e7c54a0f1e3065e9557c
Luis <garuluisnaruto@hotmail.com>
2026-03-05 21:03:14 -0600

    Added new UI to projects section

--- src/components/proyectos/Pianosmania.jsx
+++ src/components/proyectos/Pianosmania.jsx
@@ -1,13 +1,13 @@
 export const Pianosmania = () => {
     return (
         <div className="proyecto">
+            <h3>Pianosmania</h3>
+
             <div className="proyecto-imagen">
                 <img loading="lazy" src="./img/pianosmania.jpg" alt="Pianosmania" />
             </div>
 
             <div className="proyecto-informacion">
-                <h3>Pianosmania</h3>
-
                 <p>Es una pequeña página web en la que muestro vídeos mios tocando el piano. Cuenta con diseño responsivo.</p>
 
                 <p>Las tecnologías empleadas para el proyecto fueron:</p>
